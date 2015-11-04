ada3549052f4ea76c03dd37061991791fa6b0959
Knugn <david.ryman@hotmail.com>
2015-11-04 22:28:20 +0000

    Changed app to never change directory. Directory changes caused bugs previously, since working directory is global and not per thread. That is fixed now. Also changed up worker messages. Modified the returned result to include everything from the workers (didnt have drag previously). Also return the paramater settings of the job in case default values are used. Had to change up the javascript to conform to the rest api after the changes. The javascript now also generates the output in tabular format with angle, drag and lift.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 from os import path, environ
 import json
-from flask import Flask, Blueprint, abort, jsonify, request, session, url_for, redirect
+from flask import Flask, Blueprint, abort, flash, jsonify, redirect, render_template, request, session, url_for
 import settings
 from celery import Celery
 import urllib2
@@ -16,6 +16,9 @@ import fileutil
 app = Flask(__name__)
 app.config.from_object(settings)
 
+origwd = os.getcwd()
+
+run_sh_path = os.path.abspath('./run.sh')
 airfoil_path = os.path.abspath('./airfoil')
 local_data_path = os.path.abspath('./local/data/')
 local_geo_path = os.path.join(local_data_path,'geo/')
@@ -34,7 +37,8 @@ default_speed=10
 default_T=1
 
 task_ids = []
-task_dict = {}
+job_tasks_dict = {}
+job_input_dict = {}
 
 def make_celery(app):
     celery = Celery(app.import_name, broker=app.config['CELERY_BROKER_URL'])
@@ -51,152 +55,63 @@ def make_celery(app):
 celery = make_celery(app)
 
 @celery.task(name="tasks.airfoil")
-#<<<<<<< HEAD
-#def airfoil(angle, speed, job_id):
-#    print "working on angle "+str(angle)
-#   #static variables needed for gmsh/airfoil
-#    n_angles = 1
-#   n_nodes = 200
-#    n_levels = 0
-#    num_samples = 10
-#    visc = 0.0001
-#    T = 1
-#    
-#    origWD = os.getcwd()
-#    
-#    fileutil.mkdir_ifnexists(local_data_path)
-#    fileutil.mkdir_ifnexists(local_geo_path)
-#    fileutil.mkdir_ifnexists(local_msh_path)
-#    fileutil.mkdir_ifnexists(local_xml_path)
-#    fileutil.mkdir_ifnexists(local_results_path)
-#    
-#    #for angle in xrange(min_angle, (max_angle+1)):
-#    filename_format = 'r{0}a{1}n{2}'.format(n_levels, angle, n_nodes)+'{0}'
-#    subprocess.call(['./run.sh', str(angle), str(angle), str(n_angles), str(n_nodes), str(n_levels)])
-#    #filename_msh = "r"+str(n_levels)+"a"+str(angle)+"n"+str(n_nodes)+".msh"
-#    #filename_xml = "r"+str(n_levels)+"a"+str(angle)+"n"+str(n_nodes)+".xml"
-#    msh_file_path = os.path.join(local_msh_path, filename_format.format('.msh'))
-#    xml_file_path = os.path.join(local_xml_path, filename_format.format('.xml'))
-#    subprocess.call(['dolfin-convert', msh_file_path, xml_file_path])
-#    print "converted dolfin to xml"
-#    
-#    print "before changed dir"
-#    #changing directories so that when airfoil is run
-#    #The results are placed in a unique folder (jod_id/angle/)
-#    #os.makedirs(job_id+"/"+str(angle))
-#    #os.chdir(job_id+"/"+str(angle)+"/")
-#    result_dir_name = filename_format.format('_samp{0}visc{1}speed{2}time{3}'.format(num_samples, visc, speed, T)).replace(".", "-")
-#    airfoil_exec_path = os.path.join(local_results_path, result_dir_name)
-#    fileutil.mkdir_ifnexists(airfoil_exec_path)
-#    os.chdir(airfoil_exec_path)
-#    print "changed dir"
-#    with open('log.txt', 'w') as log:
-#        subprocess.call([airfoil_path, str(num_samples), str(visc), str(speed), str(T), xml_file_path], stdout=log)
-#    print "ran airfoil"
-#    lift_values = []
-#    drag_values = []
-#    with open('results/drag_ligt.m') as f:
-#        for line in f:
-#            if not line.startswith("%"):
-#                values = line.split()
-#                lift_values.append(float(values[1]))
-#                drag_values.append(float(values[2]))
-#    os.chdir(origWD)
-#    
-#    #liftval_size = len(lift_values)
-#    #liftval_sum = sum(lift_values)
-#    liftforce_mean = sum(lift_values)/len(lift_values)
-#    dragforce_mean = sum(drag_values)/len(drag_values)
-#    return {'angle':angle, 'liftforce_mean':liftforce_mean, 'dragforce_mean':dragforce_mean, 'speed':speed}
-#
-#@app.route("/af")
-#def run_airfoil(min_angle=10, max_angle=20, speed=10):
-#    global task_dict
-#    task_ids = []
-#    min_angle = int(request.args.get("min_angle", min_angle))
-#    max_angle = int(request.args.get("max_angle", max_angle))
-#    speed = int(request.args.get("speed", speed))
-#    job_id = str(uuid.uuid4())
-#    #os.makedirs(job_id)
-#    for angle in xrange(min_angle, (max_angle+1)):
-#        res=airfoil.apply_async((angle, speed, job_id))
-#        task_ids.append(res.task_id)
-#    task_dict[job_id] = task_ids
-#    return redirect(url_for('show_result_airfoil', job_id=job_id))
-#    #return jsonify(job_id=job_id, minimum_angle=min_angle, maximum_angle=max_angle, result_url=url_for('show_result_airfoil', #job_id=job_id))
-#=======
 def airfoil(angle, n_nodes, n_levels, num_samples, visc, speed, T, job_id):
-    print "working on angle " +str(angle)
-    #static variables needed for gmsh/airfoil
+    print "Working on angle " + str(angle) + " ..."
     n_angles = 1
-    #n_nodes = 200
-    #n_levels = 0
-    #num_samples = 10
-    #visc = 0.0001
-    #T = 1
 
-    lift_values = []
-    origWD = os.getcwd()
-    #anglediff=$((($angle_stop-$angle_start)/$n_angles))
+    curwd = os.getcwd()
+    if curwd != origwd:
+        os.chdir(origwd)
 
     fileutil.mkdir_ifnexists(local_data_path)
     fileutil.mkdir_ifnexists(local_geo_path)
     fileutil.mkdir_ifnexists(local_msh_path)
     fileutil.mkdir_ifnexists(local_xml_path)
     fileutil.mkdir_ifnexists(local_results_path)
-
-    #subprocess.call(['/home/ubuntu/naca_airfoil/run.sh', str(angle), str(angle), str(n_angles), str(n_nodes), str(n_levels)])
-    subprocess.call(['./run.sh', str(angle), str(angle), str(n_angles), str(n_nodes), str(n_levels)])
-    #filename_msh = "r"+str(n_levels)+"a"+str(angle)+"n"+str(n_nodes)+".msh"
-    #filename_xml = "r"+str(n_levels)+"a"+str(angle)+"n"+str(n_nodes)+".xml"
-    filename_format = 'r{0}a{1}n{2}'.format(n_levels, angle, n_nodes)+'{0}'
+    
+    subprocess.call([run_sh_path, str(angle), str(angle), str(n_angles), str(n_nodes), str(n_levels)])
+    
+    filename_format = 'r{0}a{1}n{2}'.format(n_levels, angle, n_nodes)+'{0}'    
     msh_file_path = os.path.join(local_msh_path, filename_format.format('.msh'))
     xml_file_path = os.path.join(local_xml_path, filename_format.format('.xml'))
-    #print "before changed dir"
-    #changing directories so that when airfoil is run
-    #The results are placed in a unique folder (jod_id/angle/)
-    #os.makedirs(job_id+"/"+str(angle))
-    #os.chdir(job_id+"/"+str(angle)+"/")
-    #print "changed dir"
-    #subprocess.call(['dolfin-convert', '../../msh/'+str(filename_msh), str(filename_xml)])
+    print "Converting from Gmsh format (.msh) to DOLFIN XML format (.xml) ..."
     subprocess.call(['dolfin-convert', msh_file_path, xml_file_path])
-    print "converted dolfin to xml"
     
-    
-    #subprocess.call(['../../navier_stokes_solver/airfoil', str(num_samples), str(visc), str(speed)+".", str(T), filename_xml])
-    #print "ran airfoil"
-    #    with open('results/drag_ligt.m') as f:
-    #        for line in f:
-    #            if not line.startswith("%"):
-    #                values = line.split()
-    #                lift_values.append(float(values[2]))
-    
-    print "before changed dir"
+    #print "before changed dir"
     result_dir_name = filename_format.format('_samp{0}visc{1}speed{2}time{3}'.format(num_samples, visc, speed, T)).replace(".", "-")
     airfoil_exec_path = os.path.join(local_results_path, result_dir_name)
     fileutil.mkdir_ifnexists(airfoil_exec_path)
-    os.chdir(airfoil_exec_path)
-    print "changed dir"
-    with open('log.txt', 'w') as log:
-        subprocess.call([airfoil_path, str(num_samples), str(visc), str(speed), str(T), xml_file_path], stdout=log)
-    print "ran airfoil"
+    airfoil_log_path = os.path.join(airfoil_exec_path, 'log.txt')
+    
+    #print 'Changing working directory to: ' + airfoil_exec_path
+    #os.chdir(airfoil_exec_path)
+    #print "changed dir"
+    
+    print 'Running airfoil simulation ...'
+    with open(airfoil_log_path, 'w') as log:
+        subprocess.call([airfoil_path, str(num_samples), str(visc), str(speed), str(T), xml_file_path], 
+                        stdout=log, cwd=airfoil_exec_path)
+    #print "ran airfoil"
     lift_values = []
     drag_values = []
-    with open('results/drag_ligt.m') as f:
+    drag_lift_path = os.path.join(airfoil_exec_path, 'results', 'drag_ligt.m')
+    print 'Reading results from: ' + drag_lift_path
+    with open(drag_lift_path, 'r') as f:
         for line in f:
             if not line.startswith("%"):
                 values = line.split()
                 lift_values.append(float(values[1]))
                 drag_values.append(float(values[2]))
     
-    os.chdir(origWD)
-    #liftval_size = len(lift_values)
-    #liftval_sum = sum(lift_values)
-    #liftforce_mean = liftval_sum/liftval_size
-    #return {'liftforce_mean':liftforce_mean, 'speed':speed, 'angle': angle, 'job_id': job_id}
+    #print 'Restoring working directory to: ' + origwd
+    #os.chdir(origwd)
+    
     liftforce_mean = sum(lift_values)/len(lift_values)
     dragforce_mean = sum(drag_values)/len(drag_values)
-    return {'angle':angle, 'liftforce_mean':liftforce_mean, 'dragforce_mean':dragforce_mean, 'speed':speed}
+    return {'angle':angle, 
+            'liftforce_vector':lift_values, 'dragforce_vector':drag_values,
+            'liftforce_mean':liftforce_mean, 'dragforce_mean':dragforce_mean}
+            #, 'speed':speed}
     
 
 @app.route("/af")
@@ -212,42 +127,63 @@ def run_airfoil(min_angle=10, max_angle=20, n_angles=1, n_nodes=200, n_levels=0,
     visc=float(request.args.get("visc", visc))
     speed=int(request.args.get("speed", speed))
     T=int(request.args.get("T", T))
-
+    
     job_id = str(uuid.uuid4())
-    #os.makedirs(job_id)
+    job_input_dict[job_id] = {
+        'NACA':'0012',
+        'n_nodes':n_nodes,
+        'n_levels':n_levels,
+        'num_samples':num_samples,
+        'visc':visc,
+        'speed':speed,
+        'T':T
+        }
+    
     anglediff=(((max_angle-min_angle)/n_angles))
     if anglediff < 1:
         anglediff = 1
     for angle in xrange(min_angle, (max_angle+1), int(anglediff)):
         res=airfoil.apply_async((angle, n_nodes, n_levels, num_samples, visc, speed, T, job_id))
         task_ids.append(res.task_id)
-    task_dict[job_id] = task_ids
+    job_tasks_dict[job_id] = task_ids
     return jsonify(job_id=job_id, minimum_angle=min_angle, maximum_angle=max_angle)
-#>>>>>>> 9e89b3327f8462ee10fbd25d912864e384339c8f
 
 @app.route("/af/result/<job_id>")
 def show_result_airfoil(job_id):
     global task_dict
-    retvals = []
-    for task_id in task_dict[job_id]:
-        job_result = airfoil.AsyncResult(task_id).get(timeout=1000000000.0)               
-        print job_result
-        ret = {'mean_liftforce':job_result['liftforce_mean'],
-               'speed':job_result['speed'],
-               'angle':job_result['angle'],
-               'task_id': task_id}
-        retvals.append(ret)
-    return jsonify(retvals=retvals)
+    #retvals = []
+    job_result = []
+    for task_id in job_tasks_dict[job_id]:
+        task_result = airfoil.AsyncResult(task_id).get(timeout=1000000000.0)
+        #angle = task_result['angle']
+        #del task_result['angle']
+        #job_result[angle] = task_result
+        job_result.append(task_result)
+        #print job_result
+        #ret = {'mean_liftforce':job_result['liftforce_mean'],
+        #       'speed':job_result['speed'],
+        #       'angle':job_result['angle'],
+        #       'task_id': task_id}
+        #retvals.append(ret)
+    ret = {
+        'job_id':job_id,
+        'job_input':job_input_dict[job_id],
+        'job_result':job_result
+          }
+    del job_tasks_dict[job_id]
+    del job_input_dict[job_id]
+    return jsonify(ret)
+    #return jsonify(retvals=retvals)
 
-from flask import (
-    Flask,
-    abort,
-    flash,
-    redirect,
-    render_template,
-    request,
-    url_for,
-)
+#from flask import (
+#    Flask,
+#    abort,
+#    flash,
+#    redirect,
+#    render_template,
+#    request,
+#    url_for,
+#)
 
 @app.route('/index')
 def index():
--- static/js.js
+++ static/js.js
@@ -3,7 +3,6 @@ $('document').ready(function() {
 
 
     function launchJob(min_angle, max_angle, n_angles, n_nodes, n_levels, num_samples, visc, speed, time) {
-        //var url = 'http://130.238.29.134:5000/af?' +
         var ip = location.host;
         var url = 'http://' + ip +'/af?' +
         'min_angle=' + min_angle +
@@ -28,50 +27,53 @@ $('document').ready(function() {
         var ip = location.host;
         $.when(
             $.ajax({
-                //url: 'http://130.238.29.134:5000/af/result/' + jobId
                 url: 'http://'+ip+'/af/result/' + jobId
             })
         ).then(jobResult);
     }
 
     function jobInfo(info) {
-	console.log('jobinfo'  + JSON.stringify(info));
-	var jobId = info.job_id;
-	console.log('jobid: ' +jobId);
+        console.log('jobinfo'  + JSON.stringify(info));
+        var jobId = info.job_id;
+        console.log('jobid: ' +jobId);
 
-	$('#output').val('Running job ' + jobId);
+        $('#output').val('Running job ' + jobId);
 
-	checkJob(jobId);
+        checkJob(jobId);
     }
 
     function jobResult(result) {
-	console.log('jobresult '  + JSON.stringify(result));
-	var textInOutput = $('#output').val();
-	var results = result.retvals
-	var outputText = textInOutput;
-	
-	for(var i = 0; i < results.length; i++) {		
-		var angle = results[i].angle;
-		var mean_liftforce = results[i].mean_liftforce;
-		mean_liftforce = Number((mean_liftforce).toFixed(3));
-		outputText += '\nAngle ' + angle + ': Mean lift force '
-			 + mean_liftforce;
-	}
-	console.log('output text:' + outputText);
-	$('#output').val(outputText);
+        console.log('jobresult '  + JSON.stringify(result));
+        var textInOutput = $('#output').val();
+        var result = result.job_result
+        var outputText = textInOutput;
+        outputText += '\nAngle \tDrag         \tLift\n';
+        
+            for(var i = 0; i < result.length; i++) {
+                var angle = result[i].angle;
+                var mean_dragforce = result[i].dragforce_mean;
+                var mean_liftforce = result[i].liftforce_mean;
+                mean_dragforce = Number((mean_dragforce).toFixed(3));
+                mean_liftforce = Number((mean_liftforce).toFixed(3));
+                outputText += angle + '       \t' + mean_dragforce + '   \t' + mean_liftforce + '\n'
+                    //'\nAngle ' + angle + ':\n'
+                    //+ '\tMean drag force: '+ mean_dragforce,
+                    //+ '\tMean lift force: '+ mean_liftforce;
+            }
+        console.log('output text:' + outputText);
+        $('#output').val(outputText);
     }
 
     function validInput(inputs) {
-	for (var i = 0; i < inputs.length; i++) { 
-		if(inputs[i] === '') {
-			return false;
-		}		
-	}
-
-	return true;
+        for (var i = 0; i < inputs.length; i++) { 
+            if(inputs[i] === '') {
+                return false;
+            }
+        }
+        return true;
     }
 
-	$('#output').val('');
+    $('#output').val('');
 
     $("#button").click( function()
     {
@@ -85,26 +87,22 @@ $('document').ready(function() {
         var speed = $('#speed').val();
         var time = $('#time').val();
 
-	var inputs = [min_angle, max_angle, 
-		n_angles, n_nodes, n_levels, num_samples, visc, speed, time];
+        var inputs = [min_angle, max_angle, n_angles, n_nodes, n_levels, num_samples, visc, speed, time];
 
-	if (!validInput(inputs)) {
-		alert("Please fill in all fields");
-	}
-	else {
-		launchJob(min_angle, max_angle, n_angles, n_nodes, n_levels, num_samples, visc, speed, time);
-	}
-
-	     
+        if (!validInput(inputs)) {
+            alert("Please fill in all fields");
+        }
+        else {
+            launchJob(min_angle, max_angle, n_angles, n_nodes, n_levels, num_samples, visc, speed, time);
+        }
     });
-
 
 });
 
-	function isNumberKey(evt){
-	    var charCode = (evt.which) ? evt.which : event.keyCode
-	    if (charCode > 31 && (charCode < 48 || charCode > 57))
-		return false;
-	    return true;
-	}
+function isNumberKey(evt){
+    var charCode = (evt.which) ? evt.which : event.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57))
+        return false;
+    return true;
+}
 
